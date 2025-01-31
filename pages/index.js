192fdca9db75893a09ff3aa6eb99bf4ed31031ae
Victor Catuyama <67811974+victorcatuyama@users.noreply.github.com>
2025-01-31 19:53:46 +0000

    add scripts `link:check` and `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import style from './style.module.css'
+import style from "./style.module.css";
 
 function Home() {
-    return (
-        <>
-        <div className={style.container}>
-            <h1 className={style.headline}>Hello, batata! 😎🍟</h1>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className={style.container}>
+        <h1 className={style.headline}>Hello, batata! 😎🍟</h1>
+      </div>
+    </>
+  );
 }
 
 export default Home;
