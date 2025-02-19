d3061857e1782b040c3280a930cc78f9b250c678
Victor Catuyama <67811974+victorcatuyama@users.noreply.github.com>
2025-02-19 18:31:28 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  // Updated At Tests
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // Postgres Version Test
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  // Max Connections Text
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
+
+  // Opened Connections Text
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
